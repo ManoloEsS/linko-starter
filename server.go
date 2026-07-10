df32d56bccd2516a1690733164f75e3850f8332b
ManoloEsS <manolo.estrada.s@gmail.com>
2026-07-10 15:16:25 -0600

    feat: add request identifier middleware to server
    - Gets request id from request and sets it on response writer
    - If no request id is found a randomly generated number is set as one

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
 	"errors"
 	"fmt"
 	"io"
@@ -46,7 +47,7 @@ func newServer(store store.Store, port int, cancel context.CancelFunc, logger *s
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%d", port),
-		Handler: requestLogger(logger)(mux),
+		Handler: requestIdentifier((requestLogger(logger)(mux))),
 	}
 
 	s := &server{
@@ -140,6 +141,7 @@ func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 				slog.Int("request_body_bytes", spyReader.bytesRead),
 				slog.Int("response_status", spyWriter.statusCode),
 				slog.Int("response_body_bytes", spyWriter.bytesWritten),
+				slog.String("request_id", spyWriter.Header().Get("X-Request-ID")),
 			}
 
 			if logCtx.Username != "" {
@@ -153,6 +155,20 @@ func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 			logger.Info("Served request", attrs...)
 		})
 	}
+}
+
+func requestIdentifier(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		reqID := r.Header.Get("X-Request-ID")
+		if reqID == "" {
+			reqID = rand.Text()
+		}
+
+		w.Header().Set("X-Request-ID", reqID)
+		r.Header.Set("X-Request-ID", reqID)
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 func httpError(ctx context.Context, w http.ResponseWriter, status int, err error) {
